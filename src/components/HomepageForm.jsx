ff63f62c080f818dd6ffa30b1392b1b7b08b6ba4
PRJ <53118772+PranjayPathak@users.noreply.github.com>
2022-08-30 00:36:38 +0530

    Add state for input

--- src/components/HomepageForm.jsx
+++ src/components/HomepageForm.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const HomepageForm = () => {
+
+    const [sessionId, setSessionId] = useState('');
+    const [userName, setUserName] = useState('');
+
+    const createNewRoom = () => {
+        console.log("creating new room");
+        const uuid = uuidv4();
+        console.log(uuid);
+        setSessionId(uuid);
+    }
+
+    const joinRoom = () => {
+        console.log("join room");
+    }
     return (
         <div className='homepage_form'>
 
             <div className='form'>
                 <div className='form__input_container' >
-                    <input className='form__input paragraph' id='room_id' placeholder=' ' />
+                    <input value={sessionId} onChange={(e) => setSessionId(e.target.value)} className='form__input paragraph' id='room_id' placeholder=' ' />
                     <label className='form__label paragraph' htmlFor="room_id">Session ID</label>
                 </div>
-                {/* <br /><br /><br /><br /><br /> */}
                 <div className='form__input_container'>
-                    <input className='form__input paragraph' id='room_id' placeholder=' ' />
+                    <input value={userName} onChange={(e) => setUserName(e.target.value)} className='form__input paragraph' id='room_id' placeholder=' ' />
                     <label className='form__label paragraph' htmlFor="room_id">User Name</label>
                 </div>
 
-                <button className='form__btn paragraph'>Join Room</button>
+                <button onClick={joinRoom} className='form__btn paragraph'>Join Room</button>
                 <span className='paragraph'>or</span>
-                <button className='form__btn paragraph form__btn-login'>Create New Room</button> 
+                <button onClick={createNewRoom} className='form__btn paragraph form__btn-login'>Create New Room</button>
             </div>
         </div>
     )
